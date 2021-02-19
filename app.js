4d3ebe19514c1a400e19e66333968c684cf29b50
MosaKasem <thescorpion119@gmail.com>
2021-02-19 21:14:48 +0100

    index2 fresh experiment

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ new Vue({
     el: '#app',
     data: {
         title: 'Testing Vue',
+        wage: 10,
+        name: `Write your name`,
         url: 'http://youtube.com',
         classes: ['one', 'two']
     },
     methods: {
         greet() {
             return `Hey there ${this.title}`
-        }
+        },
+        changeWage(amount) { this.wage += amount }
     }
 })
