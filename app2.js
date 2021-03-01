e54f5197afe70f3ea1cd3d42953cf83dff3afca4
MosaKasem <thescorpion119@gmail.com>
2021-03-01 22:34:58 +0100

    still experimenting

--- app2.js
+++ app2.js
@@ -2,7 +2,22 @@ new Vue({
     el: '#app',
     data: {
         title: 'Testing',
+        coords: {
+            x: 0,
+            y: 0
+        },
+        name: 'wee'
     },
     methods: {
+        logEvent(e) {
+            console.log('e: ', e);
+        },
+        logCoords(e) {
+            this.coords.x = e.offsetX
+            this.coords.y = e.offsetY
+        },
+        updateName(e) {
+            this.name = e.target.value
+        }
     }
 })
