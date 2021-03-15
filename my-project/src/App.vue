ffea01e2c328221b1c50cd02b9b3d35b1a0649ba
MosaKasem <thescorpion119@gmail.com>
2021-03-15 18:47:59 +0100

    event listening added

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,12 +1,42 @@
 <template>
   <div id="app">
     <router-view/>
+    <Navbar />
+    <AllFriends :friends="friends" v-on:delete="deleteFriend" />
+    <OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
+import AllFriends from './components/AllFriends'
+import OnlineFriends from './components/OnlineFriends'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar: Navbar,
+    AllFriends: AllFriends,
+    OnlineFriends: OnlineFriends
+  },
+  data () {
+    return {
+      friends: [
+        {name: 'Mario', online: true},
+        {name: 'Luigi', online: false},
+        {name: 'Toad', online: true},
+        {name: 'Bowa', online: false}
+      ]
+    }
+  },
+  methods: {
+    deleteFriend (payload) {
+      console.log('payload: ', payload)
+      this.friends = this.friends.filter(friend => {
+        return friend.name !== payload.name
+      })
+    } 
+  }
 }
 </script>
 
