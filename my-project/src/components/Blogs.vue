5d91a9612f2ca0a34ea2759a9d1767d1c94e6dc1
MosaKasem <thescorpion119@gmail.com>
2021-03-15 20:18:26 +0100

    cleaning life cycle

--- my-project/src/components/Blogs.vue
+++ my-project/src/components/Blogs.vue
@@ -1,7 +1,6 @@
 <template>
     <div id="blogs">
-        <h2>{{ blogTitle }}</h2>
-        <button @click="changeTitle"> Update Title </button>
+        <h2>Blogs</h2>
     </div>
 </template>
 <script>
@@ -10,22 +9,14 @@ export default {
   name: 'Blogs',
   data () {
     return {
-      blogTitle: 'Blogs'
+
     }
   },
   methods: {
-    changeTitle () {
-      this.blogTitle = 'we'
-    }
-  },
-  beforeCreate () {
-    alert('before')
+
   },
   created () {
-    alert('created')
-  },
-  beforeUpdate () {
-    alert('before update')
+
   }
 }
 </script>
